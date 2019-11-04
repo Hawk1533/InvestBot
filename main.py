e31ee91d99f58a87f14cb5b2e5121f7be22ea5e6
Andey Vasilchenko <andrew-hawk@yandex.ru>
2019-11-04 03:39:45 +0300

    3

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ import json
 import seaborn as sns
 from threading import Thread
 
+#8dd805eb8832dd154ace676690
+#7b}RnA6%
+
 class BotHandler:
 
     def __init__(self, token):
@@ -44,16 +47,17 @@ class BotHandler:
             last_update = get_result[-1]
             flag = True
         else:
-            last_update = get_result[len(get_result)]
+            #last_update = get_result[len(get_result)]
             flag = False
 
         return last_update, flag
 
 class MyThread(Thread):
-    def __init__(self, interval, func):
+    def __init__(self, interval, func, delta):
         Thread.__init__(self)
         self.interval = interval
         self.func = func
+        self.delta = delta
     
     def run(self):
         while(1):
@@ -61,13 +65,14 @@ class MyThread(Thread):
             for key in figi:
                 dif = self.func(figi[key])
                 print(dif)
-                for chat_id in user_ids:
-                    greet_bot.send_message(chat_id, key + str(dif) + '%')
+                if abs(dif) >= delta:
+                    for chat_id in user_ids:
+                        greet_bot.send_message(chat_id, key +"  " + str(dif) + '%')
 
-def create_threads(funcs, intervals):
+def create_threads(funcs, intervals, deltas):
     for i in range(len(funcs)):
         name = "Thread #%s" % (i+1)
-        my_thread = MyThread(intervals[i], funcs[i])
+        my_thread = MyThread(intervals[i], funcs[i], deltas[i])
         my_thread.start()
         print ('Tread', name, 'started')
 
@@ -78,11 +83,58 @@ def min1_dif(figi):
                                      interval = '1min')
 
     try:
-        ans = ans.to_dict()['payload']['candles'][0]
+        ans = ans.to_dict()['payload']['candles'][-1]
         return (ans['c'] - ans['o'])/ans['o'] * 100
     except:
-        return -1
-    
+        return 0
+def min15_dif(figi):
+    ans = client.market.market_candles_get(figi = figi, 
+                                     _from = (datetime.now() - timedelta(minutes=20)).isoformat()+'+03:00',
+                                     to = datetime.now().isoformat()+'+03:00' ,
+                                     interval = '15min')
+
+    try:
+        ans = ans.to_dict()['payload']['candles'][-1]
+        print ('min15_dif called')
+        return (ans['c'] - ans['o'])/ans['o'] * 100
+    except:
+        return 0    
+def hour_dif(figi):
+    ans = client.market.market_candles_get(figi = figi, 
+                                     _from = (datetime.now() - timedelta(minutes=90)).isoformat()+'+03:00',
+                                     to = datetime.now().isoformat()+'+03:00' ,
+                                     interval = 'hour')
+
+    try:
+        ans = ans.to_dict()['payload']['candles'][-1]
+        print ('hour_dif called')
+        return (ans['c'] - ans['o'])/ans['o'] * 100
+    except:
+        return 0  
+def day_dif(figi):
+    ans = client.market.market_candles_get(figi = figi, 
+                                     _from = (datetime.now() - timedelta(minutes=60*48)).isoformat()+'+03:00',
+                                     to = datetime.now().isoformat()+'+03:00' ,
+                                     interval = 'day')
+
+    try:
+        ans = ans.to_dict()['payload']['candles'][-1]
+        print ('hour_dif called')
+        return (ans['c'] - ans['o'])/ans['o'] * 100
+    except:
+        return 0 
+def week_dif(figi):
+    ans = client.market.market_candles_get(figi = figi, 
+                                     _from = (datetime.now() - timedelta(minutes=60*24*14)).isoformat()+'+03:00',
+                                     to = datetime.now().isoformat()+'+03:00' ,
+                                     interval = 'week')
+
+    try:
+        ans = ans.to_dict()['payload']['candles'][-1]
+        print ('week_dif called')
+        return (ans['c'] - ans['o'])/ans['o'] * 100
+    except:
+        return 0 
 
 token_telegram = '860172291:AAHehQPDO-oFSsLnVmNvm1hPLqqKS2InWwg'
 greet_bot = BotHandler(token_telegram)  
@@ -97,18 +149,16 @@ figi = {'yandex': 'BBG006L8G4H1'}
 user_ids = set()
 
 def main():  
-    funcs = [min1_dif]
-    intervals = [10]
-    create_threads(funcs, intervals)
-
-
+    funcs = [min1_dif, min15_dif, hour_dif, day_dif, week_deef]
+    intervals = [10, 60 * 5, 60 * 20, 60 * 60 * 8, 60 * 60 * 24]
+    deltas = [0, 3, 5, 5, 5]
+    create_threads(funcs, intervals, deltas)
 
     new_offset = None
     while True:
         greet_bot.get_updates(new_offset)
 
         data, flag = greet_bot.get_last_update()
-        print(flag)
         
         if flag:
             update_id = data['update_id']
@@ -118,14 +168,6 @@ def main():
             if not(chat_id in user_ids):
                 print('New user', chat_id)
                 user_ids.add(chat_id)
-
-            try:
-                print(1)
-                #payload = {'key':yt_token,'text': data['message']['text'],'lang':'ru-en'}
-                #translate = requests.post("https://translate.yandex.net/api/v1.5/tr.json/translate",data=payload).json()['text']
-                #greet_bot.send_message(chat_id, translate)
-            except:
-                greet_bot.send_message(chat_id, "Err")
                 
             sleep(1)   
             new_offset = update_id + 1
